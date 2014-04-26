934985f6652f8c37747eefb0398c9557e2846a06
kaitokidi <bayonetts@hotmail.com>
2014-04-26 17:21:42 +0200

    Player added

---
+++ Player.hpp
@@ -0,0 +1,45 @@
+/******************************Player.hpp**********************************/
+#ifndef PLAYER
+#define PLAYER
+
+#include "utils.hpp"
+#include <SFML/Graphics.hpp>
+
+class LD48;
+
+class Player {
+
+	public:
+		//Constructor
+		Player();
+
+		//Destructor
+		~Player();
+
+		//recieve deltatime
+		void update();
+
+		//draw the player
+		void draw();
+
+		//return the position
+		sf::Vector2f getPosition();
+		
+		setDeBuff(deBuff dB);
+		setPowerUp(powerUp pU);
+
+	private:
+		//Punter al pare (HAX)
+		LD48* pare;
+
+		//Position of the player
+		sf::Vector2f position;
+
+		//deBuffs and powerUps of the player 
+		std::vector<bool> deBuffs;
+		std::vector<bool> powerUps;
+
+};
+
+
+#endif
