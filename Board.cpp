4185865494766252870d6a42fd013673b5ac351b
debenoprma <coll.marce@gmail.com>
2014-04-26 19:53:29 +0200

    Some shiet

--- Board.cpp
+++ Board.cpp
@@ -1,32 +1,37 @@
 #include "Board.hpp"
 
-Board::Board(sf::Window* pw, int w, int h) {
-    height = h;
+Board::Board(LD48* g, int w) {
+    game = g;
+
+    pWindow = game->getWindow();
+
+    height = pWindow->getSize().y - TOP_MARGIN;
     width = w;
 
-    pWindow = pw;
+    collisionLayer.create(width, height, sf::Color(0,0,0));
+    whiteImage.create(width, height, sf::Color(255,255,255));
 
-    collisionLayer = sf::Image.create(w, h, sf::Color(0,0,0));
-    whiteImage = sf::Image.create(w, h, sf::Color(255,255,255));
+    bgDirt.setTexture(textureDirt);
+    bgTunnel.setTexture(textureTunnel);
 }
 
-inline sf::Rect getProperRectangle(sf::Vector2f current, sf::Vector2f prev) {
+inline sf::IntRect Board::getProperRectangle(sf::Vector2f current, sf::Vector2f prev) {
     float playerSpriteWidth, playerSpriteHeight;
 
     if (current.y < prev.y) {
-        return sf::Rect(current.x, current.y, playerSpriteWidth, prev.y + playerSpriteHeight - current.x);
+        return sf::IntRect(current.x, current.y, playerSpriteWidth, prev.y + playerSpriteHeight - current.x);
     }
     else if (current.y > prev.y) {
-        return sf::Rect(prev.x, prev.y, playerSpriteWidth, current.y + playerSpriteHeight - prev.x);
+        return sf::IntRect(prev.x, prev.y, playerSpriteWidth, current.y + playerSpriteHeight - prev.x);
     }
     else if (current.x < prev.x) {
-        return sf::Rect(current.x, current.y, prev.x + playerSpriteWidth - current.x, playerSpriteHeight);
+        return sf::IntRect(current.x, current.y, prev.x + playerSpriteWidth - current.x, playerSpriteHeight);
     }
     else if (current.x > prev.x) {
-        return sf::Rect(prev.x, prev.y, current.x + playerSpriteWidth - prev.x, playerSpriteHeight);
+        return sf::IntRect(prev.x, prev.y, current.x + playerSpriteWidth - prev.x, playerSpriteHeight);
     }
     else {
-        return sf::Rect(0,0,0,0);
+        return sf::IntRect(0,0,0,0);
     }
 }
 
@@ -54,17 +59,26 @@ void Board::update() {
 void Board::draw() {
     sf::Image tunnelTextImage;
 
-    tunnelTextImage = textureTunnel.copyToImage();
+    tunnelTextImage = bgTunnel.getTexture().copyToImage();
+    collisionLayer.createMaskFromColor(sf::Color::White);
+    tunnelTextImage.copy(collisionLayer,0,0,sf::IntRect(0, 0, collisionLayer.getSize().x, collisionLayer.getSize().y));
+    tunnelTextImage.createMaskFromColor(sf::Color::Black);
+    textureTunnel.loadFromImage(tunnelTextImage, sf::IntRect(0, 0, tunnelTextImage.getSize().x, tunnelTextImage.getSize().y));
+
+    bgTunnel.setTexture();
+
+    pWindow->draw(bgDirt);
+    pWindow->draw(bgTunnel);
 }
 
-boardType Board::getBoardType(sf::Vector2f pos) {
+boardType Board::getBoardType(sf::Vector2f pos, ) {
     //full digged blocked qtt_boardType
-
-    sf::Color pixelColor = collisionLayer.getPixel(pos.x, pos.y);
 
     if(pos.x > width || pos.x < 0 || pos.y < 0 || pos.y > height) {
         return blocked;
     }
+
+    sf::Color pixelColor = collisionLayer.getPixel(pos.x, pos.y);
 
     if(pixelColor == sf::Color::Black){
         return full;
--- Board.hpp
+++ Board.hpp
@@ -1,6 +1,8 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#define TOP_MARGIN 50
+
 #include "utils.hpp"
 #include "Resources.hpp"
 
@@ -10,16 +12,20 @@ class Board {
 private:
     LD48* game;
 
+    sf::Vector2f BoardPosition;
     int height;
     int width;
 
     sf::Image collisionLayer;
     sf::Image whiteImage;
 
+    sf::Sprite bgDirt;
+    sf::Sprite bgTunnel;
+
     sf::Vector2f currPlayerPos[2];
     sf::Vector2f prevPlayerPos[2];
 
-    sf::Window* pWindow;
+    sf::RenderWindow* pWindow;
 public:
     Board();
     void update();
