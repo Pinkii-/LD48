3755923e985bb7e71aee67b1f2d24d49bd32bbb6
kaitokidi <bayonetts@hotmail.com>
2014-05-03 01:18:15 +0200

    Updating utils again

--- utils.hpp
+++ utils.hpp
@@ -1,47 +1,53 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-//SFML (Simple Fast Media Library)
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-
 //STL (Standard Toolkit Library)
-#include <cmath>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
 #include <map>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <cstring>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
 
+//SFML (Simple Fast Media Library)
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+//Constant variables
 const int N_STARTING_OBJECTS = 5;
+
+//Typedefinitions
 typedef std::vector<bool> VBool;
 typedef std::vector<float> VFloat;
 
+//Game States
 enum state {
-    menu, help, credits, playing, unstarted, winned, selectMaxPoints
+    menu, help, credits, playing, unstarted, winned, selectMaxPoints, qtt_state
 };
 
-// Per afegir un debuff, afegirlo despres del dB_speed o es
-// trenca massa <3
+/* Per afegir un debuff, afegirlo despres del dB_speed
+ o es trenca massa </3 */
+//Collectibles -> PowerUps, DeBuffs and Points
 enum collectible {
-    pU_Speed, pU_Strength, pU_Phantom,
-    dB_Speed, dB_Strength, qtt_buff,
+    pU_Speed, pU_Strength, pU_Phantom, dB_Speed, dB_Strength, qtt_buff,
     pO_one, pO_two, pO_three, qtt_collectible
 };
 
+//Directions that players can take
 enum dir {
-    up, down, right, left, none
+    up, down, right, left, none, qtt_dir
 };
 
+//States that the board can have
 enum boardType {
     full, digged, blocked, qtt_boardType
 };
